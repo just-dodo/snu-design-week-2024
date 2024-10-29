7f0d4e5f9d4c507a7aacfe0e1807242ccfd4512d
junelibre <tx0203@gmail.com>
2024-10-29 19:22:52 +0900

    feat: 모바일 메인 화면, 네브바

---
+++ client/icons/CloseMenu.tsx
@@ -0,0 +1,8 @@
+export default function CloseMenu() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37" fill="none">
+      <path d="M7.55664 7.37109L29.5562 29.3711" stroke="#FFEFD3" strokeWidth="1.5" strokeLinecap="round" />
+      <path d="M29.5547 7.37109L7.55518 29.3711" stroke="#FFEFD3" strokeWidth="1.5" strokeLinecap="round" />
+    </svg>
+  );
+}
