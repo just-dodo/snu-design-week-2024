8b9aa4f89a486f636af00cbe98d93052121215eb
junelibre <tx0203@gmail.com>
2024-10-29 19:35:50 +0900

    fix: 오버레이 레이어 이슈 해결

--- client/components/mobile-menu-overlay.tsx
+++ client/components/mobile-menu-overlay.tsx
@@ -16,29 +16,9 @@ export default function MobileMenuOverlay({ isVisible, setIsVisible }: { isVisib
   if (!isVisible) return null;
 
   return (
-    <div className="fixed left-0 top-0 w-full h-full z-50 backdrop-blur-[5px]">
-      <div className="absolute right-[24px] top-[24px] z-30" onClick={() => setIsVisible(false)}>
-        <CloseMenu />
-      </div>
-      <div className="flex flex-col justify-center items-center w-full h-full gap-[30px]">
-        {linkList.map((link, index) => (
-          <div key={index}>
-            <Link href={`/${link === 'home' ? '' : link}`} onClick={() => setIsVisible(false)}>
-              <div className="text-secondary text-[31px] font-[700]">
-                {link.toUpperCase()}
-              </div>
-            </Link>
-          </div>
-        ))}
-        <div className="flex flex-row gap-[20px]">
-          <Link href="/search" onClick={() => setIsVisible(false)}>
-            <SearchIcon color='#FFEFD3' />
-          </Link>
-          <InstagramIcon />
-        </div>
-      </div>
+    <>
       {router.pathname !== '/' && (
-        <div className="absolute w-full h-full overflow-hidden">
+        <div className="absolute w-full h-full overflow-hidden z-10">
           <Image
             src="/img/mobile-main-animation.webp"
             alt="main-page-animation"
@@ -49,6 +29,30 @@ export default function MobileMenuOverlay({ isVisible, setIsVisible }: { isVisib
           />
         </div>
       )}
-    </div>
+      <div className="fixed left-0 top-0 w-full h-full z-20 backdrop-blur-[5px]">
+        <div className="absolute right-[24px] top-[24px] z-30" onClick={() => setIsVisible(false)}>
+          <CloseMenu />
+        </div>
+        <div className="absolute w-full h-full left-0 top-0">
+          <div className="flex flex-col justify-center items-center w-full h-full gap-[30px] z-30">
+            {linkList.map((link, index) => (
+              <div key={index}>
+                <Link href={`/${link === 'home' ? '' : link}`} onClick={() => setIsVisible(false)}>
+                  <div className="text-secondary text-[31px] font-[700]">
+                    {link.toUpperCase()}
+                  </div>
+                </Link>
+              </div>
+            ))}
+            <div className="flex flex-row gap-[20px]">
+              <Link href="/search" onClick={() => setIsVisible(false)}>
+                <SearchIcon color='#FFEFD3' />
+              </Link>
+              <InstagramIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
