2947f720a9e6fab68c27a72d950d9c9198e07be8
Dohyeon Park <dodo41142727@gmail.com>
2024-10-29 19:36:26 +0900

    Edit for pct (#30)

--- client/components/pc-title.tsx
+++ client/components/pc-title.tsx
@@ -4,11 +4,12 @@ interface Props {
   imgsrc: string;
   width?: number;
   height?: number;
+  className?: string;
 }
 
-export default function PCTitle({ imgsrc, width=252, height=95 }: Props) {
+export default function PCTitle({ imgsrc, width=252, height=95, className }: Props) {
   return (
-    <div className="w-auto h-auto relative hidden md:block">
+    <div className={`w-auto h-auto relative hidden md:block ${className}`}>
       <Image src={imgsrc} alt="title" width={width} height={height}/>
     </div>
   );
