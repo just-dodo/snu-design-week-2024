0409d9fa106f62cbd175265d3a708670562812d5
junelibre <tx0203@gmail.com>
2024-10-31 06:36:23 +0900

    feat(people): people mobile design

--- client/pages/people/_components/peoplecard.tsx
+++ client/pages/people/_components/peoplecard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import _useWindowSize from "utils/useWindowSize";
 
 export type PeopleCardProps = {
   name: string;
@@ -15,14 +16,16 @@ interface Props {
 }
 
 export default function PeopleCard({ people }: Props) {
+  const { isMobileView } = _useWindowSize();
+
   return (
-    <div className="flex flex-col items-center justify-between border-2 border-primary rounded-[16px] w-[154px] h-[203px] relative">
-      <div className="absolute top-[10px] left-[10px] w-[26px] h-[26px] border-2 border-primary rounded-full"></div>
-      <div className="flex flex-col mt-[30px] items-center gap">
-        <div className="text-[20px] font-[700] text-primary h-[26px]">
+    <div className="flex flex-col items-center justify-between border-2 border-primary rounded-[16px] md:w-[154px] w-[165px] md:h-[203px] h-[161px] relative">
+      <div className="absolute top-[10px] left-[10px] md:w-[26px] w-[18px] md:h-[26px] h-[18px] border-2 border-primary rounded-full"></div>
+      <div className="flex flex-col mt-[22px] items-center gap">
+        <div className="md:text-[20px] text-[16px] font-[700] text-primary h-[26px]">
           {people?.name}
         </div>
-        <div className="text-[20px] font-[500] text-primary h-[26px]">
+        <div className="md:text-[20px] text-[16px] font-[500] text-primary h-[26px]">
           {people?.englishName}
         </div>
       </div>
@@ -31,7 +34,7 @@ export default function PeopleCard({ people }: Props) {
           <Link
             key={work.id}
             href={`/works/${work.id}`}
-            className="w-[134px] h-[44px] bg-[#00BD84] rounded-[10px] text-secondary font-[500] text-[17px] text-center leading-[44px]"
+            className="md:w-[134px] w-[142px] md:h-[44px] h-[33px] bg-[#00BD84] rounded-[10px] text-secondary font-[500] md:text-[17px] text-[14px] text-center md:leading-[44px] leading-[33px]"
           >
             {work.class}
           </Link>
