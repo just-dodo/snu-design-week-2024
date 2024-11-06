fc9d6e286a47a2ebe9d8987a4797e66fc2691f73
junelibre <tx0203@gmail.com>
2024-11-06 16:32:16 +0900

    fix(search-and-instagram): 우측 패딩 수정

--- client/components/search-and-instagram.tsx
+++ client/components/search-and-instagram.tsx
@@ -23,14 +23,14 @@ export default function SearchAndInstagram() {
   };
 
   return (
-    <div className='flex flex-row justify-end px-[20px] py-[20px] gap-[20px] z-20'>
+    <div className='flex flex-row justify-end px-[40px] py-[20px] gap-[20px] z-20'>
       <form onSubmit={handleSubmit}>
         <div className='relative z-20 hover:cursor-pointer' onClick={handleSearchClick}>
           <SearchIcon color={`${isSearchOpen ? '#E22613': '#FFEFD3'}`}/>
         </div>
         <input
           placeholder="Find your projects!"
-          className={`absolute z-10 outline-none top-[110px] right-[82px] p-3 text-[24px] bg-[#FFEFD3] text-[#E22613] rounded-full transition-all duration-500 ease-in-out placeholder:text-[24px] placeholder:text-[#E22613] ${isSearchOpen ? 'w-[380px] opacity-100' : 'w-0 opacity-0'
+          className={`absolute z-10 outline-none top-[110px] right-[102px] p-3 text-[24px] bg-[#FFEFD3] text-[#E22613] rounded-full transition-all duration-500 ease-in-out placeholder:text-[24px] placeholder:text-[#E22613] ${isSearchOpen ? 'w-[380px] opacity-100' : 'w-0 opacity-0'
             }`}
           onChange={handleChange}
           value={input}
