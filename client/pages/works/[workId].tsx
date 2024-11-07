29537233764e2ba397e1ba29b400a717b252ec86
Dohyeon Park <dodo41142727@gmail.com>
2024-11-07 16:47:23 +0900

    Fix margin (#40)
    * Fix margin
    
    * Fix icon

--- client/pages/works/[workId].tsx
+++ client/pages/works/[workId].tsx
@@ -382,7 +382,7 @@ export default function WorkPage(
             router.back();
           }}
         >
-          <HiArrowNarrowLeft color={theme.colors.primary} size={48} />
+          <ArrowLeft color={theme.colors.primary} size={48} />
         </div>
         <div className="flex flex-1 flex-row items-center justify-between h-fit md:min-h-[52px] md:mb-0 ">
           <div
@@ -449,7 +449,7 @@ export default function WorkPage(
           <div className="w-full h-full bg-gradient-to-b from-transparent to-secondary"></div>
         </div>
         <XWrapper>
-          <div className="w-full  p-6 md:p-0">
+          <div className="w-full p-6 md:p-0">
             <div className="w-full h-[210px] md:h-[440px]" />
             <div className="w-full h-fit relative z-10 min-h-[72px]">
               <div
@@ -547,7 +547,9 @@ export default function WorkPage(
               OTHER WORKS
             </h1>
           </div>
-          <div className="grid grid-cols-3 gap-4 mb-[10px] md:mb-5">{randomWorkCards}</div>
+          <div className="grid grid-cols-3 gap-4 mb-[10px] md:mb-5">
+            {randomWorkCards}
+          </div>
         </div>
         <div className="w-full flex flex-row justify-between items-center text-primary">
           <Link href={`/works/${prevId}`}>
