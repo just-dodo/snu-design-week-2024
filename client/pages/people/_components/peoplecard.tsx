5d577dc3bbb77a5407be96810e48d4407d46c177
junelibre <tx0203@gmail.com>
2024-11-10 06:23:31 +0900

    fix(mobile-people): border width 조정

--- client/pages/people/_components/peoplecard.tsx
+++ client/pages/people/_components/peoplecard.tsx
@@ -19,8 +19,8 @@ export default function PeopleCard({ people }: Props) {
   const { isMobileView } = _useWindowSize();
 
   return (
-    <div className="flex flex-col items-center justify-between border-2 border-primary rounded-[16px] md:w-[154px] w-[165px] md:h-[203px] h-[161px] relative">
-      <div className="absolute top-[10px] left-[10px] md:w-[26px] w-[18px] md:h-[26px] h-[18px] border-2 border-primary rounded-full"></div>
+    <div className="flex flex-col items-center justify-between md:border-2 border-[1.5px] border-primary rounded-[16px] md:w-[154px] w-[165px] md:h-[203px] h-[161px] relative">
+      <div className="absolute top-[10px] left-[10px] md:w-[26px] w-[18px] md:h-[26px] h-[18px] md:border-2 border-[1.5px] border-primary rounded-full"></div>
       <div className="flex flex-col mt-[22px] items-center gap">
         <div className="md:text-[20px] text-[16px] font-[700] text-primary h-[26px]">
           {people?.name}
