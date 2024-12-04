a8f81712cabfb2ad320546b23d9e0f0f2e649342
JuneLee <tx0203@gmail.com>
2024-12-05 05:37:42 +0900

    feat(program): 수정사항 반영, 세부페이지 디자인
    * feat(program): 행사 갯수 축소에 따른 카테고리 수정
    
    * feat(program): 모바일 세부 페이지 디자인
    
    * feat(program): PC 세부 페이지 디자인

---
+++ client/pages/program/_components/SNUD-youtube.tsx
@@ -0,0 +1,21 @@
+export default function SNUDYoutube() {
+  const Details = "일정 | 9.11 - 10.29\n대상 | 졸업전시 관람객\n\n졸업전시자 전체를 대상으로 진행한 인터뷰. 각 질문에 대한 주차별로 달라지는 졸업전시자들의 대답을 통해, 전시주간이 다가오는 것에 대한 그들의 심경변화를 살펴보자. 각 인터뷰 속 질문을 통해 작품설명에는 나오지 않는 졸업전시자들의 이야기와 졸업전시에 대한 정보, 디자인에 대한 유용한 팁 등을 얻을 수도 있다."
+  return (
+    <div className="px-[16px] py-[20px] w-full flex flex-col gap-[20px]">
+      <div className="w-full h-[200px] md:h-[540px] bg-gray-300">
+        Skeleton
+      </div>
+      <div className="flex flex-col gap-[4px]">
+        <div className="md:w-[430px] md:text-[32px] text-[20px] text-primary font-[700] md:tracking-[-0.32px]">
+          유튜브
+        </div>
+        <div className="md:w-[430px] md:text-[32px] text-[20px] text-primary font-[700] md:tracking-[-0.32px]">
+          SNUD Youtube
+        </div>
+      </div>
+      <div className="md:w-[430px] md:pt-[4px] text-[15px] text-primary font-[400] leading-[24px] whitespace-pre-line md:tracking-[-0.165px]">
+        {Details}
+      </div>
+    </div>
+  )
+}
--- client/pages/program/index.tsx
+++ client/pages/program/index.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react';
 import { NextPage } from 'next';
 import PCTitle from 'components/pc-title';
-import OpeningDay from './_components/opening-day';
-import Workshop from './_components/workshop';
 import HiDay from './_components/hi-day';
-import TalkConcert from './_components/talk-concert';
 import _useWindowSize from 'utils/useWindowSize';
 import Category from './_components/category-mobile';
 import SearchAndInstagram from 'components/search-and-instagram';
+import SNUDYoutube from './_components/SNUD-youtube';
 
 const Program: NextPage = () => {
-  const [selectedCategory, setSelectedCategory] = useState("OPENING DAY");
+  const [selectedCategory, setSelectedCategory] = useState("SNUD Youtube");
 
   const { isMobileView } = _useWindowSize();
 
   if (isMobileView) {
     return (
       <div className="mb-[50px] w-full flex flex-col items-center gap-[12px] pt-[50px] min-h-screen px-[24px]">
-        <Category kor="오프닝 데이" eng="OPENING DAY" date="11.28" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}>
-          <OpeningDay />
+        <Category kor="유튜브" eng="SNUD Youtube" date="9.11 - 10.29" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}>
+          <SNUDYoutube />
         </Category>
-        <Category kor="워크샵" eng="WORKSHOP" date="11.29" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}>
-          <Workshop />
-        </Category>
-        <Category kor="하이 데이" eng="HI DAY" date="11.30" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}>
+        <Category kor="하이 데이" eng="HI DAY" date="11.29" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}>
           <HiDay />
-        </Category>
-        <Category kor="토크 콘서트" eng="TALK CONCERT" date="12.1" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}>
-          <TalkConcert />
         </Category>
       </div>
     );
@@ -40,29 +32,19 @@ const Program: NextPage = () => {
       </div>
       <div className="pt-[88px] mb-[100px] w-[900px] flex flex-col items-center gap-[20px]">
         <PCTitle imgsrc="/img/pc-title-program.svg" width={294} height={93} />
-        <div className="flex flex-row justify-between w-full items-center mt-[5px] mb-[12px] mx-[20px]">
-          <div onClick={() => setSelectedCategory("OPENING DAY")} className={`flex flex-col items-center w-auto cursor-pointer ${selectedCategory === "OPENING DAY" ? null : 'opacity-50'}`}>
-            <div className="text-[20px] font-[700] text-primary">OPENING DAY</div>
-            <div className="text-[16px] font-[400] text-primary">오프닝 데이</div>
-          </div>
-          <div onClick={() => setSelectedCategory("WORKSHOP")} className={`flex flex-col items-center w-auto cursor-pointer ${selectedCategory === "WORKSHOP" ? null : 'opacity-50'}`}>
-            <div className="text-[20px] font-[700] text-primary">WORKSHOP</div>
-            <div className="text-[16px] font-[400] text-primary">워크샵</div>
+        <div className="flex flex-row md:gap-[100px] gap-[63.5px] items-center mt-[5px] mb-[12px]">
+          <div onClick={() => setSelectedCategory("SNUD Youtube")} className={`flex flex-col items-center w-auto cursor-pointer ${selectedCategory === "SNUD Youtube" ? null : 'opacity-50'}`}>
+            <div className="text-[20px] font-[700] text-primary">SNUD Youtube</div>
+            <div className="text-[16px] font-[400] text-primary">유튜브</div>
           </div>
           <div onClick={() => setSelectedCategory("HI DAY")} className={`flex flex-col items-center w-auto cursor-pointer ${selectedCategory === "HI DAY" ? null : 'opacity-50'}`}>
             <div className="text-[20px] font-[700] text-primary">HI DAY</div>
             <div className="text-[16px] font-[400] text-primary">하이 데이</div>
           </div>
-          <div onClick={() => setSelectedCategory("TALK CONCERT")} className={`flex flex-col items-center w-auto cursor-pointer ${selectedCategory === "TALK CONCERT" ? null : 'opacity-50'}`}>
-            <div className="text-[20px] font-[700] text-primary">TALK CONCERT</div>
-            <div className="text-[16px] font-[400] text-primary">토크 콘서트</div>
-          </div>
         </div>
         <div className="w-[900px] mt-[30px]">
-          {selectedCategory === "OPENING DAY" && <OpeningDay />}
-          {selectedCategory === "WORKSHOP" && <Workshop />}
+          {selectedCategory === "SNUD Youtube" && <SNUDYoutube />}
           {selectedCategory === "HI DAY" && <HiDay />}
-          {selectedCategory === "TALK CONCERT" && <TalkConcert />}
         </div>
       </div>
     </div>
