b8d1a74a651af8091ff1d37b1f28431a14cf311c
junelibre <tx0203@gmail.com>
2024-12-05 04:15:48 +0900

    feat(program): 행사 갯수 축소에 따른 카테고리 수정

--- client/pages/program/_components/SNUD-youtube.tsx
+++ client/pages/program/_components/SNUD-youtube.tsx
@@ -1,11 +1,11 @@
-export default function TalkConcert() {
+export default function SNUDYoutube() {
   return (
     <div className="px-[16px] py-[20px] w-full flex flex-row gap-[40px]">
       <div className="w-[430px] text-[15px] text-primary font-[400] leading-[24px] text-justify">
-        Talk Concert
+        SNUD:Youtube
       </div>
       <div className="w-[430px] text-[15px] text-primary font-[400] leading-[24px] tracking-[-0.165px] text-justify">
-        토크 콘서트
+        SNUD:유튜브
       </div>
     </div>
   )
