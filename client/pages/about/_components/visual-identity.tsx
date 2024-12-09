8456c4c33c982e5c1c1746bd7ee33ef5c15c73e2
junelibre <tx0203@gmail.com>
2024-12-10 03:53:44 +0900

    feat(about): visual identity 본문과 포스터 교체와 삽입

--- client/pages/about/_components/visual-identity.tsx
+++ client/pages/about/_components/visual-identity.tsx
@@ -19,16 +19,14 @@ export default function VisualIdentity() {
         }
       </div>
       <div className="flex justify-center">
-        <Image
-          src={
-            isMobileView
-              ? "/img/visual-identity-apple-caterpillar-mobile.svg"
-              : "/img/visual-identity-apple-caterpillar.svg"
-          }
-          alt={isMobileView ? "visual-identity-mobile" : "visual-identity"}
-          width={isMobileView ? 226 : 354}
-          height={isMobileView ? 113 : 209}
-        />
+        <div className="md:w-[497px] w-[233px] shadow-[0px_3px_14px_0px_rgba(0,0,0,0.30)]">
+          <Image
+            src="/img/main-poster.png"
+            alt="main-poster"
+            width={isMobileView ? 233 : 497}
+            height={isMobileView ? 330 : 703}
+          />
+        </div>
       </div>
       <div className="w-full grid md:grid-cols-2 md:gap-[40px] gap-[20px] text-justify">
         <div className="md:w-[430px] w-full text-[15px] text-primary font-[400] leading-[24px] text-justify">
@@ -39,8 +37,16 @@ export default function VisualIdentity() {
             {VISUALIDENTITY.eng[0]}
           </div>
         }
+        <div className="md:w-[430px] w-full text-[15px] text-primary font-[400] leading-[24px] text-justify">
+          {VISUALIDENTITY.kor[1]}
+        </div>
+        {!isMobileView &&
+          <div className="md:w-[430px] w-full text-[15px] tracking-[-0.165px] text-primary font-[400] leading-[24px] text-justify">
+            {VISUALIDENTITY.eng[1]}
+          </div>
+        }
       </div>
-      <div className="flex flex-col text-primary md:text-[32px] text-[20px] tracking-[-0.2px] md:tracking-normal">
+      <div className="flex flex-col mt-[40px] md:mt-[0px] text-primary md:text-[32px] text-[20px] tracking-[-0.2px] md:tracking-normal">
         <div className="font-[600] md:font-[700]">
           모션 포스터
         </div>
@@ -52,20 +58,23 @@ export default function VisualIdentity() {
         }
       </div>
       <div className="flex justify-center">
-        <Image
-          src="/img/motion-poster.svg"
-          alt="motion-poster"
-          width={497}
-          height={703}
-        />
+        <div className="md:w-[497px] w-[233px] shadow-[0px_3px_14px_0px_rgba(0,0,0,0.30)]">
+          <Image
+            src="/img/motion-poster.gif"
+            alt="motion-poster"
+            width={isMobileView ? 233 : 497}
+            height={isMobileView ? 330 : 703}
+            unoptimized={true}
+          />
+        </div>
       </div>
       <div className="w-full grid md:grid-cols-2 md:gap-[40px] gap-[20px] text-justify">
         <div className="md:w-[430px] w-full text-[15px] text-primary font-[400] leading-[24px] text-justify">
-          {VISUALIDENTITY.kor[1]}
+          {VISUALIDENTITY.kor[2]}
         </div>
         {!isMobileView &&
           <div className="md:w-[430px] w-full text-[15px] tracking-[-0.165px] text-primary font-[400] leading-[24px] text-justify">
-            {VISUALIDENTITY.eng[1]}
+            {VISUALIDENTITY.eng[2]}
           </div>
         }
       </div>
